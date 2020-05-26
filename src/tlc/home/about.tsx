8e19726e040bb6db951ca2b4ba1abafa52d279f1
Zach Gangwer <zach@gangwerz.xyz>
2020-05-26 11:10:09 -0700

    Updated About Me content, fixed a typo

--- src/tlc/home/about.tsx
+++ src/tlc/home/about.tsx
@@ -5,19 +5,34 @@ class About extends React.Component {
         return (
             <div id="about">
                 <p className="first">
-                    I am a Software Engineer currently located in Eugene, OR. I
-                    have experience as a IT Support Tech, in addition to my
-                    Programming, Linux, and DevOps skills. I am currently
-                    looking for an in-office or remote opportunity that will
-                    allow me to exercise my coding skills and relocate to a
-                    larger city either.
+                    Hello! My name is Zach Gangwer and I am a Full-Stack
+                    Software Developer. I have worn many hats, with experience
+                    in IT support, Linux, and DevOps in addition to programming.
+                </p>
+                <p>
+                    The website you are viewing was written in React and hosted
+                    on AWS, and features icons from{" "}
+                    <a href="https://fontawesome.com/" target="blank">
+                        <u>Font Awesome</u>.
+                    </a>
                 </p>
                 <p className="last">
-                    This website is a space for me to experiment, so the content
-                    should be considered dynamic and volitile. If you have any
-                    suggestions, found any bugs, or just want to check my
-                    development progess go checkout my{" "}
-                    <a href="https://gitlab.com/gangwerz/website">
+                    This site is a space for me to experiment, so the content
+                    should be considered dynamic, volitile, and may be
+                    (temporarily) full of{" "}
+                    <a
+                        href="https://en.wikipedia.org/wiki/Spaghetti_code"
+                        target="blank"
+                    >
+                        <u>pasta</u>
+                    </a>
+                    . If you have any styling suggestions, found any bugs, or
+                    just want to check my development plan and progress, go
+                    checkout my{" "}
+                    <a
+                        href="https://gitlab.com/gangwerz/website"
+                        target="blank"
+                    >
                         <u>Gitlab</u>
                     </a>{" "}
                     repo.
