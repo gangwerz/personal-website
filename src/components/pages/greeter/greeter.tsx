18c08092fea2b74818a312b23c19894cc803d946
Zach Gangwer <zach@gangwerz.xyz>
2020-06-04 08:36:32 -0700

    Parallax Component created with 'styled components.' However the image, when passed, does not display.

--- src/components/pages/greeter/greeter.tsx
+++ src/components/pages/greeter/greeter.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import Badge from "./badge";
 import badges from "./badgeData";
+import Parallax from "../../shared/parallax";
 
 class Greeter extends React.Component {
     render() {
         return (
             <div id="greeter" className="page">
+                <Parallax
+                    img_url={
+                        "https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_960_720.jpg"
+                    }
+                ></Parallax>
                 <h1>Zach Gangwer</h1>
                 <div id="badges">{this.parseIcons()}</div>
             </div>
@@ -19,6 +25,7 @@ class Greeter extends React.Component {
         for (let i = 0; i < badges.length; i++) {
             out.push(
                 <Badge
+                    key={i}
                     name={badges[i].name}
                     link={badges[i].link}
                     icon={badges[i].icon}
