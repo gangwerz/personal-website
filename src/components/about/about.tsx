1e72215c236e7558222479f997f5edb2e952979d
Zach Gangwer <zach@gangwerz.xyz>
2020-06-04 11:10:00 -0700

    Straight port of 'index.css' to 'styled-components.' Global styles and custom properties moved from 'index.css' to 'globals.css'

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
-import aboutMe from "./aboutData";
+import styled from "styled-components";
+
+import paragraphs from "./paragraphs";
+
+const AboutWrapper = styled.section`
+    background-color: var(--secondary_light);
+    display: flex;
+    justify-content: center;
+    padding: 1.5rem;
+    min-width: calc(100vw - 3rem);
+    min-height: calc(100vh - 3rem);
+    text-align: center;
+    text-align: left;
+    font-size: 1.7rem;
+    white-space: pre-line;
+`;
 
 class About extends React.Component {
     render() {
         return (
-            <div id="about" className="page">
-                {aboutMe()}
-            </div>
+            <AboutWrapper id="about" className="page">
+                {paragraphs()}
+            </AboutWrapper>
         );
     }
 }
